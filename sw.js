84c77bdd5082446d695910b5edc7d5c6b694bf33
Catchase <catchase365@gmail.com>
2026-06-18 05:24:39 +0800

    PWA: 智能双线离线固化 sw 更新至 v_1781731479605

---
+++ sw.js
@@ -0,0 +1,22 @@
+var C='v_1781731479605';
+var assets = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(C).then(c => c.addAll(assets)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== C && caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  let requestToFetch = e.request;
+  if (e.request.mode === 'navigate') {
+    requestToFetch = new Request(e.request, { cache: 'reload' });
+  }
+  e.respondWith(
+    fetch(requestToFetch)
+      .then(res => {
+        if (res && res.status === 200) {
+          var resClone = res.clone();
+          caches.open(C).then(c => c.put(e.request, resClone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(cached => cached || new Response('Offline', { status: 503 })))
+  );
+});
